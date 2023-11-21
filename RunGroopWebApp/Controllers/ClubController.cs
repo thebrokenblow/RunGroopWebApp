6d4543008154037f1c2defdfc77527b756302837
Artem <artkrasov@gmail.com>
2023-11-22 01:42:57 +0300

    Added data display in View

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers;
 
 public class ClubController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    public ClubController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var clubs = _context.Clubs.ToList();
+        return View(clubs);
     }
 }
--- RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/Controllers/RaceController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using RunGroopWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RunGroopWebApp.Controllers;
 
 public class RaceController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    public RaceController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var races = _context.Races.ToList();
+        return View(races);
     }
 }
